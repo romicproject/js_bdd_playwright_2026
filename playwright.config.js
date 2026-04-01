61615e447f8bb61226996bb36f8426e85add0bea
Romeo Gandea <romeo.gandea@endava.com>
2026-04-01 17:27:35 +0300

    Include milliseconds in LOG_RUN_ID

--- playwright.config.js
+++ playwright.config.js
@@ -7,7 +7,8 @@ function hhmmssSSS(d = new Date()) {
   const hh = String(d.getHours()).padStart(2, "0");
   const mm = String(d.getMinutes()).padStart(2, "0");
   const ss = String(d.getSeconds()).padStart(2, "0");
-  return `${hh}-${mm}-${ss}`;
+  const ms = String(d.getMilliseconds()).padStart(3, "0");
+  return `${hh}-${mm}-${ss}-${ms}`;
 }
 
 if (!process.env.LOG_RUN_ID) {
